6ea346de15f87b78608ded002401f7cfa11f437d
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2020-12-19 14:29:05 +0530

    Add files via upload

---
+++ count_occurrence_dic.py
@@ -0,0 +1,12 @@
+string="MISSISSIPPI"
+count = {"M":0,"I":0,"S":0,"P":0}
+for i in string:
+	if i == "M":
+		count['M'] = count['M']+1
+	elif i == "I":
+		count['I'] = count['I']+1
+	elif i == "S":
+		count['S'] = count['S']+1
+	elif i == "P":
+		count['P'] = count['P']+1
+print(count)
